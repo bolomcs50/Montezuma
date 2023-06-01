13a05c3a184bf1df493950b4d10eddf154b46aba
Michele Bolognini <michele.bolognini@fastwebnet.it>
2023-06-01 21:31:41 +0200

    remove out line

--- test/solveMates.cpp
+++ test/solveMates.cpp
@@ -6,14 +6,13 @@
 int main(int argc, char **argv){
     std::stringstream commandStream;
     montezuma::Engine engine(commandStream, std::cout);
-    std::ifstream problemsFile("res/fens2.fen", std::ios::in);
+    std::ifstream problemsFile("res/fens3.fen", std::ios::in);
     std::string line, command;
     commandStream << "uci\n";
     while(getline(problemsFile, line)){
         command = "ucinewgame\nposition fen " + line + "\ngo\nquit\n";
         commandStream << command;
         engine.protocolLoop();
-        std::cout << command << std::endl;
     }
     return 0;
 }
