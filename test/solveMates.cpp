b0d8b1c213a91b50f1f1c52bf3b43f1b4b2d3845
Michele Bolognini <michele.bolognini@fastwebnet.it>
2023-06-05 21:12:24 +0200

    Check solutions against other engine

--- test/solveMates.cpp
+++ test/solveMates.cpp
@@ -1,18 +1,51 @@
 #include "engine.h"
 #include <iostream>
 #include <sstream>
+#include <fstream>
+#include <catch2/catch_test_macros.hpp>
 
 
-int main(int argc, char **argv){
-    std::stringstream commandStream;
-    montezuma::Engine engine(commandStream, std::cout);
-    std::ifstream problemsFile("res/fens3.fen", std::ios::in);
-    std::string line, command;
+TEST_CASE("Engine solves mates"){
+    std::stringstream commandStream, outputStream;
+    montezuma::Engine engine(commandStream, outputStream);
+    std::string position, solution, command, attempt;
     commandStream << "uci\n";
-    while(getline(problemsFile, line)){
-        command = "ucinewgame\nposition fen " + line + "\ngo\nquit\n";
-        commandStream << command;
-        engine.protocolLoop();
+
+    SECTION("Mates in 2"){
+        std::ifstream problemsFile("res/MatesIn2.txt", std::ios::in);
+        while(getline(problemsFile, position)){
+            getline(problemsFile, solution);
+            command = "ucinewgame\nposition fen " + position + "\ngo\nquit\n";
+            commandStream << command;
+            engine.protocolLoop();
+
+            while(getline(outputStream, attempt)){
+                if (attempt.find("mate") != std::string::npos){
+                    std::cout << "Position: " << position << std::endl << "Attempt: " << attempt << std::endl << "Solution: " << solution << std::endl << std::endl;
+                    CHECK(attempt.find(solution) != std::string::npos);
+                    outputStream.str(std::string());
+                    break;
+                }
+            }
+        }
     }
-    return 0;
+
+    SECTION("Mates in 3"){
+        std::ifstream problemsFile("res/MatesIn3.txt", std::ios::in);
+        while(getline(problemsFile, position)){
+            getline(problemsFile, solution);
+            command = "ucinewgame\nposition fen " + position + "\ngo\nquit\n";
+            commandStream << command;
+            engine.protocolLoop();
+
+            while(getline(outputStream, attempt)){
+                if (attempt.find("mate") != std::string::npos){
+                    std::cout << "Position: " << position << std::endl << "Attempt: " << attempt << std::endl << "Solution: " << solution << std::endl << std::endl;
+                    CHECK(attempt.find(solution) != std::string::npos);
+                    outputStream.str(std::string());
+                    break;
+                }
+            }
+        }
+    }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -3,16 +3,6 @@
 #include <sstream>
 #include <catch2/catch_test_macros.hpp>
 
-// int main(int argc, char **argv){
-    // montezuma::Engine engine;
-    // std::stringstream commandStream;
-    // commandStream << "uci\nposition fen r1b2k1r/ppppq3/5N1p/4P2Q/4PP2/1B6/PP5P/n2K2R1 w - - 1 0\ngo\nquit\n";
-    // std::cout << engine.protocolLoop(commandStream) << std::endl;
-    // commandStream << "uci\nposition fen 1rb4r/pkPp3p/1b1P3n/1Q6/N3Pp2/8/P1P3PP/7K w - - 1 0\ngo\nquit\n";
-    // std::cout << engine.protocolLoop(commandStream) << std::endl;
-//     return 0;
-// }
-
 TEST_CASE("Engine initializes correctly"){
     std::stringstream iStream, oStream;
     montezuma::Engine engine(iStream, oStream);
