7c520803ebedfa3b4b31a1bf7881c27ba244c55a
Michele Bolognini <michele.bolognini@fastwebnet.it>
2024-02-16 23:56:40 +0100

    Delete old files

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./tester.sh | ./build/Montezuma
--- tester.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sleep 1
-echo "ucinewgame"
-cat res/m8n2.txt | grep -E ".*/.*/.*/.*/" | sed 's/\(.*\)/position fen \1\ngo/'
-echo "quit"
-
-
