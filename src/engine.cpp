9ba14edfc73786d83846a34c1998f6679fdde514
Michele Bolognini <michele.bolognini@fastwebnet.it>
2024-02-21 15:27:38 +0100

    Initial idea

--- src/engine.cpp
+++ src/engine.cpp
@@ -8,6 +8,9 @@
 #include <algorithm>
 #include "engine.h"
 
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+
 namespace montezuma {
 
 Engine::Engine(std::istream& inputStream, std::ostream& outputStream):
