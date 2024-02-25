ba1529ed21b9e39f9025a9c4ec17d924d92783ea
Michele Bolognini <michele.bolognini@fastwebnet.it>
2024-02-25 16:15:12 +0100

    Limit number of problems

--- test/solveMates.sh
+++ test/solveMates.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/expect
 
-set timeout 10
+set timeout 20
 set input_file [lindex $argv 0]
 puts "Reading problems from $input_file"
 
@@ -12,7 +12,7 @@ expect "uciok"
 expect "readyok"
 
 set file [open $input_file r]
-
+set solvedProblems 0
 while {[gets $file fenLine] != -1} {
     if {[gets $file solLine] != -1} {
         set firstMove [string range $solLine 0 [expr {[string first " " $solLine] - 1}]]
@@ -23,7 +23,10 @@ while {[gets $file fenLine] != -1} {
             "bestmove " {puts "Wrong answer, expecting $firstMove in FEN $fenLine" ; exit 1}
             timeout {puts "Timed out while waiting: expected move $firstMove FEN $fenLine" ; exit 1}
         }
-        # expect "bestmove"
+        incr solvedProblems
+    }
+    if {$solvedProblems > 40} { #TODO Once the engine is faster, remove problems counter
+        exit 0
     }
 }
 
